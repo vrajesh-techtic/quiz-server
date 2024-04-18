5a291cec5c0cdfd46b5c0cc47b7e3abd14d1b256
Vrajesh Patel <vrajesh542003@gmail.com>
2024-04-18 08:04:31 +0530

    Updated Department CRUD

--- src/controller/deptCRUD.js
+++ src/controller/deptCRUD.js
@@ -1,5 +1,7 @@
 const { ObjectId } = require("mongodb");
 const { dept } = require("../models/deptDBSchema");
+const { decryptToken } = require("../middleware/authMiddleware");
+const { admin } = require("../models/adminSchema");
 
 /*
 
@@ -9,19 +11,47 @@ Required Parameters:
 
 */
 
-const createDept = async (req, res) => {
-  const deptName = req.body.deptName;
-
+const isAdmin = async (id) => {
   try {
-    const query = await dept.create({ deptName: deptName });
+    const adminId = new ObjectId(id);
+    const query = await admin.findById(adminId, { _id: 0, password: 0 });
 
-    if (query !== null) {
-      res.send({ status: true, message: "Department created successfully!" });
-    } else {
-      res.send({ status: false, message: "Department not created !" });
+    if (await query) {
+      return query;
     }
   } catch (error) {
     res.send({ status: false, message: error.message });
+  }
+};
+
+const createDept = async (req, res) => {
+  const deptName = req.body.deptName;
+  const token = req.body.token;
+  const id = decryptToken(token);
+
+  if (!id.status) {
+    res.send({ status: false, message: id.message });
+  }
+
+  const checkAdmin = await isAdmin(id.token);
+  console.log("checkAdmin", checkAdmin);
+  if (checkAdmin) {
+    try {
+      const query = await dept.create({
+        deptName: deptName,
+        admin_id: id.token,
+      });
+
+      if (query !== null) {
+        res.send({ status: true, message: "Department created successfully!" });
+      } else {
+        res.send({ status: false, message: "Department not created !" });
+      }
+    } catch (error) {
+      res.send({ status: false, message: error.message });
+    }
+  } else {
+    res.send({ status: false, message: "User does not exists!" });
   }
 };
 
